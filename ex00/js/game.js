234f25849f4052d75af177cd38b83b9f5a6083b9
Conrad KLEINESPEL <ckleines@e1r7p17.42.fr>
2014-04-11 05:39:49 +0200

    added grid

--- ex00/js/game.js
+++ ex00/js/game.js
@@ -2,14 +2,42 @@
 	var canvas = document.getElementById("myCanvas");
 	var ctx = canvas.getContext("2d");
 
+	var factor = {
+		x: canvas.width / 150,
+		y: canvas.height / 100
+	};
+
+	function grid(x, y)
+	{
+		var color = "#999999";
+		var i = -1;
+		while (++i <= y)
+		{
+			ctx.strokeStyle = color;
+			ctx.moveTo(0, i * factor.y);
+			ctx.lineTo(canvas.width, i * factor.y);
+			ctx.stroke();
+		}
+		var i = -1;
+		while (++i <= x)
+		{
+			ctx.strokeStyle = color;
+			ctx.moveTo(i * factor.x, 0);
+			ctx.lineTo(i * factor.x, canvas.height);
+			ctx.stroke();
+		}
+	}
+
+	grid(150, 100);
+
 	$.get("/ex00/index.php?action=objects", function(data)
 	{
 		data.forEach(function(el, i) {
 			if (el.alive) {
-				var img = new Image(el.w * 10, el.h * 10);
+				var img = new Image(el.w * factor.x, el.h * factor.y);
 				img.src = el.sprite;
 				img.onload = function() {
-					ctx.drawImage(img, el.x * 10, el.y * 10, el.w * 10, el.h * 10);
+					ctx.drawImage(img, el.x * factor.x, el.y * factor.y, el.w * factor.x, el.h * factor.y);
 				}
 			}
 		});
--- ex00/templates/game.php
+++ ex00/templates/game.php
@@ -2,7 +2,7 @@
 <html>
 <title>Asteroberg le retour du badass !</title>
 <body>
-	<canvas id="myCanvas" width="1500" height="1000" style="border:1px solid #000000;">
+	<canvas id="myCanvas" width="2000" height="1300" style="border:1px solid #000000;">
 	</canvas>
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js"></script>
 	<script type="text/javascript" src="js/game.js"></script>	
