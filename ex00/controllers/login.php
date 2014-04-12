c139dc61dcd0fe15de3f899a1e058f112b7f429d
Sousa Dany <danysousa.sd@gmail.com>
2014-04-12 17:27:25 +0200

    sign in/out

--- ex00/controllers/login.php
+++ ex00/controllers/login.php
@@ -2,10 +2,8 @@
 
 return function() {
 
-	app()->get('session')->set('login', NULL);
-	if ( $_POST && $_POST['register_login']
-		&& $_POST['register_password']
-		&& ($_POST['register_submit'] == 'FIGHT'))
+	if ($_POST && array_key_exists("register_login", $_POST)
+		&& array_key_exists("register_password", $_POST))
 	{
 		$pwd = hash('whirlpool', $_POST['register_password']);
 		$verif = app()->get('db')->query("SELECT name FROM user WHERE name LIKE ?", array($_POST['register_login']));
@@ -17,9 +15,20 @@ return function() {
 			app()->get('session')->set('id_usr', $id);
 		}
 	}
-	if (!app()->get('session')->get('login'))
+	else if ($_POST && $_POST['login_login']
+			&& $_POST['login_password'])
+	{
+		$pwd = hash('whirlpool', $_POST['login_password']);
+		$verif = app()->get('db')->query("SELECT id FROM user WHERE name LIKE ? AND pwd LIKE ?", array($_POST['login_login'], $pwd));
+		if ($verif)
+		{
+			app()->get('session')->set('login', $_POST['login_login']);
+			app()->get('session')->set('id_usr', $verif[0]['id']);
+		}
+	}
+	if (!app()->get('session')->get('login') || !app()->get('session')->get('id_usr'))
 		echo app()->get('view')->render('login');
 	else
-		echo app()->get('view')->render('index');
+		header('Location: ./index.php?action=home');
 
 };
