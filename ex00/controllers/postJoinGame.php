d476fe2f996d983201a55dcfb4f2e42d31d5b48c
Conrad KLEINESPEL <ckleines@e2r6p14.42.fr>
2014-04-12 17:36:44 +0200

    added game creation and joining

--- ex00/controllers/postJoinGame.php
+++ ex00/controllers/postJoinGame.php
@@ -2,10 +2,27 @@
 
 return function() {
 	$gameName = trim((string)$_POST['join_game_login']);
+	$login = app()->get('session')->get('login');
 
-	if ($gameName !== '') {
-		// save game
-	} else {
-		// return errors
+	if ($login === false) {
+		die('Must be logged in.');
 	}
+
+	if ($gameName === '') {
+		die('Game name cannot be empty.');
+	}
+
+	$user = app()->get('db')->queryOne("SELECT * FROM user WHERE name = ?", [$login]);
+	if ($user === null) {
+		die('Logged in user does not exist (weird, right?).');
+	}
+
+	$id = app()->get('db')->queryOne("SELECT id FROM partie WHERE name = ?", [$gameName]);
+	if ($id === null) {
+		die('Game does not exist.');
+	}
+
+	app()->get('db')->query("UPDATE user SET id_partie = ? WHERE id = ?", [$id, $user['id']]);
+
+	echo 'Successfully joined the game!';
 };
