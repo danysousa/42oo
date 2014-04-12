66258c08e9fed4b4e5d8ba41b5174d89a7a21933
Sousa Dany <danysousa.sd@gmail.com>
2014-04-12 17:02:42 +0200

    register

--- ex00/controllers/login.php
+++ ex00/controllers/login.php
@@ -1,5 +1,23 @@
 <?php
 
 return function() {
-	echo app()->get('view')->render('login');
+
+	app()->get('session')->set('login', NULL);
+	if ( $_POST && $_POST['register_login']
+		&& $_POST['register_password']
+		&& ($_POST['register_submit'] == 'FIGHT'))
+	{
+		$pwd = hash('whirlpool', $_POST['register_password']);
+		$verif = app()->get('db')->query("SELECT name FROM user WHERE name LIKE ?", array($_POST['register_login']));
+		if (!$verif)
+		{
+			app()->get('db')->query("INSERT INTO user (name, pwd) VALUES(?, ?)", array($_POST['register_login'], $pwd));
+			app()->get('session')->set('login', $_POST['register_login']);
+		}
+	}
+	if (!app()->get('session')->get('login'))
+		echo app()->get('view')->render('login');
+	else
+		echo app()->get('view')->render('index');
+
 };
