2c95f8aa2a690a9b9eb03dacaddb73bedfeca601
Conrad KLEINESPEL <ckleines@e2r6p12.42.fr>
2014-04-13 19:31:44 +0200

    Added move collisions and canvas refresh client side

--- ex00/js/game.js
+++ ex00/js/game.js
@@ -42,23 +42,21 @@
 
 	function drawObjects(objects)
 	{
+		ctx.clearRect(0, 0, 3000, 3000);
 		objects.forEach(function(el, i)
 		{
-			console.dir(el);
-			if (1) {
-				var img = new Image(el.ship_w * factor.x, el.ship_h * factor.y);
-				img.style.outline = '1px solid red';
-				// set the right sprite for the given object direction
-				img.src = el.ship_sprite.replace('{{dir}}', getDirIndex(el.ship_dir));
-				img.onload = function()
-				{
-					// west or east
-					if (el.direction % 2 === 0)
-						//             image,  posX,                 posY,                   width,                height
-						ctx.drawImage(img, el.ship_posX * factor.x, el.ship_posY * factor.y, el.ship_h * factor.y, el.ship_w * factor.x);
-					else
-						ctx.drawImage(img, el.ship_posX * factor.x, el.ship_posY * factor.y, el.ship_w * factor.x, el.ship_h * factor.y);
-				}
+			var img = new Image(el.ship_w * factor.x, el.ship_h * factor.y);
+			img.style.outline = '1px solid red';
+			// set the right sprite for the given object direction
+			img.src = el.ship_sprite.replace('{{dir}}', getDirIndex(el.ship_dir));
+			img.onload = function()
+			{
+				// west or east
+				if (el.direction % 2 === 0)
+					//             image,  posX,                 posY,                   width,                height
+					ctx.drawImage(img, el.ship_posX * factor.x, el.ship_posY * factor.y, el.ship_h * factor.y, el.ship_w * factor.x);
+				else
+					ctx.drawImage(img, el.ship_posX * factor.x, el.ship_posY * factor.y, el.ship_w * factor.x, el.ship_h * factor.y);
 			}
 		});
 	}
@@ -132,6 +130,7 @@
 				}).success(function(data) {
 					$scope.selectedShip.ship_dir = direction;
 					$scope.rotationDone = true;
+					drawObjects($scope.game.ships);
 				});
 			} else {
 				$scope.rotationDone = true;
@@ -143,82 +142,18 @@
 				url: 'index.php?action=postTurnSubmitMove',
 				method: "POST"
 			}).success(function(data) {
+				$scope.moved = true;
 				// if the ship was out of bounds and died
 				// if the ship hit another ship, lock it !
-				if (data === 'atomic') {
-					alert('ATOMIC BOMB ! BOUYAA!');
+				if (typeof data.x === 'undefined') {
+					alert('Your ship just died from a collision! :(');
 					return;
 				}
 				$scope.selectedShip.ship_posX = data.x;
 				$scope.selectedShip.ship_posY = data.y;
-				$scope.moved = true;
+				drawObjects($scope.game.ships);
 			});
 		}
-
-		/*
-		$http.get('/ex00/index.php?action=player').success(function(player)
-		{
-			$scope.player = player;
-		});
-		$http.get('/ex00/index.php?action=playerShips').success(function(ships)
-		{
-			$scope.playerShips = ships;
-		});
-		$http.get('/ex00/index.php?action=objects').success(function(objects)
-		{
-			ctx.clearRect(0, 0, canvas.height, canvas.width);
-			drawGrid(150, 100);
-			drawObjects(objects);
-			$scope.objects = objects;
-		});
-		*/
-
-		// document.addEventListener('keydown', function(event) 
-		// {
-		// 	if ($scope.chosenShip)
-		// 	{
-		// 	    if(event.keyCode == KEY_LEFT) 
-		// 	    {
-		// 			$http.get('/ex00/index.php?action=move&x=-1&y=0&id=').success(function(objects)
-		// 			{
-		// 				ctx.clearRect(0, 0, canvas.height, canvas.width);
-		// 				drawGrid(150, 100);
-		// 				drawObjects(objects);
-		// 				$scope.objects = objects;
-		// 			});
-		// 	    }
-		// 	    else if(event.keyCode == KEY_RIGHT) 
-		// 	    {
-		// 			$http.get('/ex00/index.php?action=move&x=1&y=0').success(function(objects)
-		// 			{
-		// 				ctx.clearRect(0, 0, canvas.height, canvas.width);
-		// 				drawGrid(150, 100);
-		// 				drawObjects(objects);
-		// 				$scope.objects = objects;
-		// 			});
-		// 	    }
-		// 	    else if(event.keyCode == KEY_UP) 
-		// 	    {
-		// 			$http.get('/ex00/index.php?action=move&x=0&y=1').success(function(objects)
-		// 			{
-		// 				ctx.clearRect(0, 0, canvas.height, canvas.width);
-		// 				drawGrid(150, 100);
-		// 				drawObjects(objects);
-		// 				$scope.objects = objects;
-		// 			}); 
-		// 	    }
-		// 	    else if(event.keyCode == KEY_DOWN) 
-		// 	    {
-		// 			$http.get('/ex00/index.php?action=move&x=0&y=-1').success(function(objects)
-		// 			{
-		// 				ctx.clearRect(0, 0, canvas.height, canvas.width);
-		// 				drawGrid(150, 100);
-		// 				drawObjects(objects);
-		// 				$scope.objects = objects;
-		// 			});
-		// 	    }
-		// 	}
-		// });
 	});
 
 })();
